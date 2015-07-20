948c0d3c51b1a65ef66e773e95b421cb674fee17
Seth <sethc.dorris@gmail.com>
2015-07-20 11:17:11 -0500

    completed the quiz on object constructors without styling

---
+++ main.js
@@ -0,0 +1,22 @@
+var Tree = function(name, leafShape, color, size) {
+	this.name = name;
+	this.leafShape = leafShape;
+    this.color = color;
+    this.size = size;
+}
+
+var redwood = new Tree("Redwood", "Oblong", "Brown", "Huge");
+var evergreen = new Tree("Evergreen", "Spikey", "Green", "Medium");
+var blackAsh = new Tree("Black Ash", "Compound", "Ash Gray", "Medium");
+var basswood = new Tree("Basswood", "Simple", "Dark Gray", "Medium");
+
+var tree_array = [redwood, evergreen, blackAsh, basswood];
+
+var treeHolderElement = document.getElementById("tree-holder");
+
+for (var i = 0; i < tree_array.length; i++) {
+	treeHolderElement.innerHTML += "<div>" + "Tree name: " + tree_array[i].name + "<br>" +
+	 "The shape of the leaf is: " + tree_array[i].leafShape + " <br>" +
+	 "The color of the tree is: " + tree_array[i].color + "<br>" + 
+	 "The size of the tree is: " + tree_array[i].size + "<br>" + "</div>";
+}
